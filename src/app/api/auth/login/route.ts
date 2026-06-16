c723fd0a719ad2c9447cb6c52088403d4f5fbb79
august-duarte <augustoleiteduarte@gmail.com>
2026-06-15 22:18:00 -0300

    refactor: finished porting login route to .ts

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -2,41 +2,34 @@ import sql from "@/lib/db";
 import { loginValidation } from "@/lib/validation";
 import bcrypt from "bcrypt";
 import { createToken } from "@/lib/auth/jwt";
+import { NextResponse } from "next/server";
 
-export const POST = async (req: Request, res: Response) => {
+export const POST = async (req: Request) => {
   try {
-    //valida as info
-    const { error } = loginValidation(req.body);
+    const { error } = loginValidation(await req.json());
     if (error) {
-      return res.status(400).json({ message: error.details[0].message })
+      return NextResponse.json({ error: error.details[0].message }, { status: 400 })
     }
 
-    //mesma coisa em /register, menos nome
-    const { email, password } = req.body;
+    const { email, password } = await req.json();
 
-    //verifica se user existe
     const [user] = await sql`
       SELECT * FROM users WHERE email = ${email}
     `
-    //se deu errado, manda a mensagem de erro
     if (!user) {
-      return res.status(400).json({ message: 'User doesn\'t exist. Please check your email and password.' })
+      return NextResponse.json({ error: 'User doesn\'t exist. Please check your email and password.' }, { status: 400 })
     }
-    //compara a senha enviada com a senha na db
     const validPassword = await bcrypt.compare(password, user.password);
 
-    //senha errada
     if (validPassword === false) {
-      return res.status(400).json({ message: 'Invalid password' });
+      return NextResponse.json({ error: 'Invalid password' }, { status: 400 })
     }
-
-    //cria o token
+    
     const token = createToken(user.id);
 
-    //se tudo deu certo, login ok
-    res.json({ token: token });
+    return NextResponse.json({ token: token }, { status: 200 })
   } catch (error) {
     console.error('Login failed', error);
-    res.status(500).json({ message: 'Something went wrong, please try again later' });
+    return NextResponse.json({ error: 'Something went wrong, please try again later' }, { status: 500 })
   }
-})
+}
