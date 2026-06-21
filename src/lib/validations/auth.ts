c48279c7f1d36da3c3db53ac16be2f842fc955d4
august-duarte <augustoleiteduarte@gmail.com>
2026-06-21 18:10:33 -0300

    refactor: create folder for the Joi validations and separate login/register and profile validations

---
+++ src/lib/validations/auth.ts
@@ -0,0 +1,19 @@
+import Joi from 'joi';
+
+export const registerValidation = (data: any) => {
+  const schema = Joi.object({
+    name: Joi.string().max(255).required(),
+    email: Joi.string().email().max(255).required(),
+    password: Joi.string().min(6).max(255).required(),
+  });
+  return schema.validate(data);
+}
+
+export const loginValidation = (data: any) => {
+  const schema = Joi.object({
+    email: Joi.string().email().max(255).required(),
+    password: Joi.string().min(6).max(255).required(),
+  });
+  return schema.validate(data);
+}
+
---
+++ src/lib/validations/profiles.ts
@@ -0,0 +1,17 @@
+import Joi from 'joi';
+
+export const updatePasswordValidation = (data: any) => {
+  const schema = Joi.object({
+    oldPassword: Joi.string().min(6).max(255).required(),
+    newPassword: Joi.string().min(6).max(255).required(),
+  });
+  return schema.validate(data);
+};
+
+export const updateProfileValidation = (data: any) => {
+  const schema = Joi.object({
+    name: Joi.string().min(2).max(255),
+    email: Joi.string().email().max(255),
+  }).or('name', 'email'); 
+  return schema.validate(data);
+};
